82772b5061d6ac8289dc52bf94c21cd58aef4867
Ibrahim <ibrahim@ibrahim.ai>
2021-08-15 11:34:16 +0300

    update postcss.config.js and package.json prod command

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,16 @@
-// postcss.config.js
+const purgecss = require('@fullhuman/postcss-purgecss')
+const cssnano = require('cssnano')
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  }
+	plugins: [
+		require('tailwindcss'),
+		require('autoprefixer'),
+		cssnano({
+			preset: 'default',
+		}),
+		purgecss({
+			content: ['./public/*.html'],
+			defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+		}),
+	],
 }
